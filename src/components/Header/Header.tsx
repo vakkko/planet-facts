3c0cfb6e78539a9bf78a9f26a658d886e447ac00
Vako Gabisonia <fgrge@gmail.com>
2025-01-09 23:11:56 +0400

    Style planet links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,62 @@
-import { NavLink, Outlet } from "react-router";
+import { NavLink, Outlet, useParams } from "react-router";
 import "./header.css";
 
 export default function Header() {
+  const params = useParams<{ planetName: string }>();
+  const planetName = params.planetName?.toLowerCase();
+
   return (
     <>
       <header>
         <p className="logo">THE PLANETS</p>
         <nav className="planets-navigation">
-          <NavLink to={"/mercury/overview"} className="planet-link">
+          <NavLink
+            to={"/mercury/overview"}
+            className={`link ${planetName === "mercury" ? "link-mercury" : ""}`}
+          >
             {" "}
             MERCURY
           </NavLink>
-          <NavLink className="planet-link" to={"venus/overview"}>
+          <NavLink
+            className={`link ${planetName === "venus" ? "link-venus" : ""}`}
+            to={"venus/overview"}
+          >
             VENUS
           </NavLink>
-          <NavLink className="planet-link" to={"earth/overview"}>
+          <NavLink
+            className={`link ${planetName === "earth" ? "link-earth" : ""}`}
+            to={"earth/overview"}
+          >
             EARTH
           </NavLink>
-          <NavLink className="planet-link" to={"mars/overview"}>
+          <NavLink
+            className={`link ${planetName === "mars" ? "link-mars" : ""}`}
+            to={"mars/overview"}
+          >
             MARS
           </NavLink>
-          <NavLink className="planet-link" to={"saturn/overview"}>
+          <NavLink
+            className={`link ${planetName === "saturn" ? "link-saturn" : ""}`}
+            to={"saturn/overview"}
+          >
             SATURN
           </NavLink>
-          <NavLink className="planet-link" to={"jupiter/overview"}>
+          <NavLink
+            className={`link ${planetName === "jupiter" ? "link-jupiter" : ""}`}
+            to={"jupiter/overview"}
+          >
             JUPITER
           </NavLink>
-          <NavLink className="planet-link" to={"uranus/overview"}>
+          <NavLink
+            className={`link ${planetName === "uranus" ? "link-uranus" : ""}`}
+            to={"uranus/overview"}
+          >
             URANUS
           </NavLink>
-          <NavLink className="planet-link" to={"neptune/overview"}>
+          <NavLink
+            className={`link ${planetName === "neptune" ? "link-neptune" : ""}`}
+            to={"neptune/overview"}
+          >
             NEPTUNE
           </NavLink>
         </nav>
