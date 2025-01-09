6074644b6ef88a6409e37036fb915d419e928f54
Vako Gabisonia <fgrge@gmail.com>
2025-01-09 21:53:01 +0400

    Remove inline style on Navlink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,76 +1,35 @@
 import { NavLink, Outlet } from "react-router";
 import "./header.css";
 
-const linkStyle = {
-  fontSize: "1.1rem",
-  fontWeight: 700,
-  lineHeight: "2.5rem",
-  letterSpacing: "0.1rem",
-  color: "white",
-  paddingTop: "2rem",
-};
-
 export default function Header() {
   return (
     <>
       <header>
         <p className="logo">THE PLANETS</p>
         <nav className="planets-navigation">
-          <NavLink
-            to={"/mercury/overview"}
-            className="planet-link"
-            style={linkStyle}
-          >
+          <NavLink to={"/mercury/overview"} className="planet-link">
             {" "}
             MERCURY
           </NavLink>
-          <NavLink
-            className="planet-link"
-            style={linkStyle}
-            to={"venus/overview"}
-          >
+          <NavLink className="planet-link" to={"venus/overview"}>
             VENUS
           </NavLink>
-          <NavLink
-            className="planet-link"
-            style={linkStyle}
-            to={"earth/overview"}
-          >
+          <NavLink className="planet-link" to={"earth/overview"}>
             EARTH
           </NavLink>
-          <NavLink
-            className="planet-link"
-            style={linkStyle}
-            to={"mars/overview"}
-          >
+          <NavLink className="planet-link" to={"mars/overview"}>
             MARS
           </NavLink>
-          <NavLink
-            className="planet-link"
-            style={linkStyle}
-            to={"saturn/overview"}
-          >
+          <NavLink className="planet-link" to={"saturn/overview"}>
             SATURN
           </NavLink>
-          <NavLink
-            className="planet-link"
-            style={linkStyle}
-            to={"jupiter/overview"}
-          >
+          <NavLink className="planet-link" to={"jupiter/overview"}>
             JUPITER
           </NavLink>
-          <NavLink
-            className="planet-link"
-            style={linkStyle}
-            to={"uranus/overview"}
-          >
+          <NavLink className="planet-link" to={"uranus/overview"}>
             URANUS
           </NavLink>
-          <NavLink
-            className="planet-link"
-            style={linkStyle}
-            to={"neptune/overview"}
-          >
+          <NavLink className="planet-link" to={"neptune/overview"}>
             NEPTUNE
           </NavLink>
         </nav>
